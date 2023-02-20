6619231939222958b0b965479c83d4fd31f12196
kennethsolomon <mr.kennethsolomon@gmail.com>
2023-02-20 23:52:50 +0800

    fix some code

--- app/Http/Requests/PropertyPostRequest.php
+++ app/Http/Requests/PropertyPostRequest.php
@@ -13,7 +13,7 @@ class PropertyPostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/api_v3.php
+++ routes/api_v3.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 // V3
 Route::get("properties", [\App\Http\Controllers\Api\V3\PropertyController::class, 'index']);
+
+Route::post("update_or_create_property", [\App\Http\Controllers\Api\V3\PropertyController::class, 'updateOrCreateProperty']);
+
+Route::delete("delete_property/{property}", [\App\Http\Controllers\Api\V3\PropertyController::class, 'destroy']);
