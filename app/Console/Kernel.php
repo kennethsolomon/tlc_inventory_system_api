3614940f7a3fe5978941b0cf7d51ac9b5bb9fa98
kennethsolomon <mr.kennethsolomon@gmail.com>
2023-04-02 08:23:40 +0800

    testing kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('property:overdue')->dailyAt('01:30');;
-        $schedule->command('maintenance:frequency')->dailyAt('02:00');;
+        $schedule->command('inspire')->everyMinute();
+        $schedule->command('property:overdue')->dailyAt('01:30');
+        $schedule->command('maintenance:frequency')->dailyAt('02:00');
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
+    info('Hello World');
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
